df6762c072172f4ec18949d238d0dd127ca6160c
ilh4msh <ilhamputrahasim@gmail.com>
2025-09-17 09:19:23 +0700

    menambahkan fungsi untuk melihat dalam format xml dan json. membuat page add product dan show product serta memperbarui halaman utama

--- main/models.py
+++ main/models.py
@@ -1,13 +1,43 @@
 from django.db import models
 
 class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ("jersey", "Jersey"),
+        ("celana", "Celana"),
+        ("sarung_tangan", "Sarung Tangan"),
+        ("sepatu", "Sepatu"),
+        ("pelindung", "Pelindung (siku/lutut/pinggul)"),
+        ("kaos_kaki", "Kaos Kaki"),
+        ("aksesoris", "Aksesoris"),
+    ]
     
-    name = models.CharField(max_length=255) 
-    price = models.IntegerField()  
-    description = models.TextField()  
-    thumbnail = models.URLField()  
-    category = models.CharField(max_length=50)  
-    is_featured = models.BooleanField(default=False)  
-    
+    SIZE_CHOICES = [
+        ('xs', 'XS'),
+        ('s', 'S'),
+        ('m', 'M'),
+        ('l', 'L'),
+        ('xl', 'XL'),
+        ('xxl', 'XXL'),
+        ('xxxl', 'XXXL'),
+    ]
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='jersey')
+    thumbnail = models.URLField()
+    price = models.IntegerField()
+    stock = models.IntegerField(blank=True, null=True)
+    size = models.CharField(max_length=20,  choices=SIZE_CHOICES, default='l')    
+    product_views = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.name
+    
+    @property
+    def is_product_trending(self):
+        return self.product_views > 20
+        
+    def increment_views(self):
+        self.product_views += 1
+        self.save()
--- main/views.py
+++ main/views.py
@@ -1,10 +1,66 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from main.forms import ProductForm  
+from main.models import Product
+from django.http import HttpResponse
+from django.core import serializers
+
 
 def show_main(request):
+    products = Product.objects.all()
+    
     context = {
         "app_name": "Goalie’s Safehouse",  # nama aplikasi
         "name": "Ilham Shahputra Hasim",
         "npm": "2406401193",
-        "class": "PBP A"   
+        "class": "PBP A",
+        "product_list": products,
     }
     return render(request, "main.html", context)
+
+def create_product(request):
+    form = ProductForm(request.POST or None) 
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_product.html", context)
+
+
+def show_product(request, id):
+    product = get_object_or_404(Product, pk=id)
+    product.increment_views()
+    
+    product = get_object_or_404(Product, pk=id)
+
+    context = {
+        'product': product
+    }
+
+    return render(request, "show_product.html", context)
+
+    
+def show_xml(request):
+    products = Product.objects.all()
+    xml_data = serializers.serialize("xml", products)
+    return HttpResponse(xml_data, content_type="application/xml")
+
+def show_json(request):
+    products = Product.objects.all()
+    json_data = serializers.serialize("json", products)
+    return HttpResponse(json_data, content_type="application/json")
+
+def show_xml_by_id(request, product_id):
+    product = Product.objects.filter(pk=product_id)
+    if not product.exists():
+        return HttpResponse(status=404)
+    xml_data = serializers.serialize("xml", product)
+    return HttpResponse(xml_data, content_type="application/xml")
+
+def show_json_by_id(request, product_id):
+    product = Product.objects.filter(pk=product_id)
+    if not product.exists():
+        return HttpResponse(status=404)
+    json_data = serializers.serialize("json", product)
+    return HttpResponse(json_data, content_type="application/json")    
